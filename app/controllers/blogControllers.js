98c79e98915e4432b5d2f9293d19332a8794191e
IM AJIJUL <imajijulislam@gmail.com>
2024-11-07 20:45:28 +0600

    2nd commit

---
+++ app/controllers/blogControllers.js
@@ -0,0 +1,19 @@
+// Create Route
+export const creatBlog = (req, res)=>{
+    res.send("Blog Created Successfully")
+}
+
+// Read Route
+export const readBlog = (req, res)=>{
+    res.send("Blog Read Successfully")
+}
+
+// Update Route
+export const updateBlog = (req, res)=>{
+    res.send("Blog Update Successfully")
+}
+
+// Delete Route
+export const deleteBlog = (req, res)=>{
+    res.send("Blog Delete Successfully")
+}
--- router/api.js
+++ router/api.js
@@ -1,15 +1,15 @@
 import express from "express";
-import * as controller from "../app/controllers/controllers.js"
+import * as controller from "../app/controllers/blogControllers.js"
 
 const router = express.Router();
 
-router.post("/create-blog", controller.createRoute)
+router.post("/create-blog", controller.creatBlog)
 
-router.get("/read-blog", controller.readRoute)
+router.get("/read-blog", controller.readBlog)
 
-router.put("/update-blog", controller.updateRoute)
+router.put("/update-blog", controller.updateBlog)
 
-router.delete("/delete-blog", controller.deleteRoute)
+router.delete("/delete-blog", controller.deleteBlog)
 
 
 export default router;
